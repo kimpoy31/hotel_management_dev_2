87e3399ee03a9d03137c9b676b4acc5992f33627
Kim Pinanonang <pinanonangkim000@gmail.com>
2025-03-07 16:09:32 +0800

    WIP setup admin page

--- resources/js/Pages/Admin.tsx
+++ resources/js/Pages/Admin.tsx
@@ -1,4 +1,5 @@
 import Card from "@/components/Card";
+import { Link } from "@inertiajs/react";
 import React from "react";
 
 interface Props {}
@@ -6,7 +7,14 @@ interface Props {}
 const Admin = ({}: Props) => {
     return (
         <div className="w-full flex items-center flex-col">
-            <Card></Card>
+            <Card>
+                <div className="flex justify-between">
+                    <h2 className="card-title">Employee manager</h2>
+                    <Link href="" className="btn btn-sm btn-accent">
+                        Add employee
+                    </Link>
+                </div>
+            </Card>
         </div>
     );
 };
--- resources/js/components/Card.tsx
+++ resources/js/components/Card.tsx
@@ -1,18 +1,9 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
-const Card = () => {
+const Card = ({ children }: { children: ReactNode }) => {
     return (
         <div className="card flex-none w-full max-w-7xl bg-base-100 card-md shadow-sm">
-            <div className="card-body">
-                <h2 className="card-title">Medium Card</h2>
-                <p>
-                    A card component has a figure, a body part, and inside body
-                    there are title and actions parts
-                </p>
-                <div className="justify-end card-actions">
-                    <button className="btn btn-primary">Buy Now</button>
-                </div>
-            </div>
+            <div className="card-body">{children}</div>
         </div>
     );
 };

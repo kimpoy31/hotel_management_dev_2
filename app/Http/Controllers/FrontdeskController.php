e1c747b61fd707aa46f163895406ddf2dab09c1b
kimpoy <kimpoyyy31@gmail.com>
2025-03-16 15:01:15 +0800

    handle checking in

--- app/Http/Controllers/FrontdeskController.php
+++ app/Http/Controllers/FrontdeskController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\GeneralSetting;
 use App\Models\InventoryItem;
 use App\Models\Rate;
 use App\Models\Room;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use Inertia\Inertia;
 
@@ -23,7 +27,6 @@ class FrontdeskController extends Controller
         ]);
     }
 
-
     public function check_in (Request $request){
         // Validation rules
         $validator = Validator::make($request->all(), [
@@ -40,7 +43,6 @@ class FrontdeskController extends Controller
             'total_payment' => ['required', 'numeric', 'min:0.01'],
         ]);
 
-
         // If validation fails, return a 422 error with validation errors
         if ($validator->fails()) {
             return to_route('frontdesk.room.form', $request->room_id)->withErrors($validator);
@@ -48,13 +50,12 @@ class FrontdeskController extends Controller
 
         
         $room = Room::find($request->input('room_id'));
-        $room_inclusions = $room->room_inclusions;
+
+
+        // * HANDLE ROOM ADDITIONS -> subtract additions from inventory -> available
         $room_additions = json_decode($request->input('room_additions'), true) ?? [];
     
-        // Merge both arrays
-        $all_items = array_merge($room_inclusions, $room_additions);
-
-        foreach($all_items as $item){
+        foreach($room_additions as $item){
             $inventoryItem = InventoryItem::find($item['item_id']);
             $quantity_to_update = $item['quantity'];
             
@@ -63,6 +64,35 @@ class FrontdeskController extends Controller
                 'in_use' => $inventoryItem->in_use += $quantity_to_update,
             ]);
         }
-        
+
+        // * SAVE IF ID PICTURE IS PROVIDED
+        if ($request->hasFile('id_picture') && $request->file('id_picture')->isValid()) {
+            $idPicturePath = Storage::disk('public')->putFile('id_pictures', $request->file('id_picture'));
+        }
+
+        // GENERAL SETTINGS
+        $generalSettings = GeneralSetting::find(1);
+
+        $transaction = Transaction::create([
+            'transaction_officer' => Auth::user()->fullname,
+            'check_in' => $request->input('check_in'),
+            'expected_check_out' => $request->input('expected_check_out'),
+            'number_of_hours' => $request->input('number_of_hours'),
+            'number_of_days' => $request->input('number_of_days'),
+            'rate' => $request->input('rate'),
+            'room_number' => $request->input('room_number'),
+            'customer_name' => $request->input('customer_name'),
+            'customer_address' => $request->input('customer_address'),
+            'customer_contact_number' => $request->input('customer_contact_number'),
+            'id_picture_path' => $idPicturePath ?? null,
+            'room_additions' => json_decode($request->input('room_additions'),true),
+            'total_payment' => $request->input('total_payment'),
+            'overtime_charge' => $generalSettings->overtime_charge,
+        ]);
+
+        $room->update([
+            'active_transaction' => $transaction->id,
+            'room_status' => 'occupied'
+        ]);
     }
 }

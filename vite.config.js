737565683a87509fcd72f785071701744f0c4fc6
kimpoy <kimpoyyy31@gmail.com>
2025-04-13 01:33:17 +0800

    configure vite for network access

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,24 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
+            input: "resources/js/app.tsx",
             refresh: true,
         }),
         react(),
         tailwindcss(),
     ],
+    server: {
+        host: "192.168.1.6",
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            protocol: "ws",
+            host: "192.168.1.6",
+        },
+    },
 });

d1536c6fe1eb2356dc8a9e5006711a7bd147a11f
kimpoy <kimpoyyy31@gmail.com>
2025-04-14 00:06:59 +0800

    refactor saving of room

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Rate;
 use App\Models\Reservation;
 use App\Models\Room;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -11,19 +13,22 @@ class ReservationController extends Controller
 {
     public function reserve_room(Request $request)
     {
-        $room = Room::find($request->input('reserved_room_id'));
         $reservationId = $request->input('reservation_id');
-
+       
         // If reservationId exists, find the reservation; otherwise, create a new instance
         $reservation = Reservation::find($reservationId) ?? new Reservation();
+
+        // Parse as PHT and convert to UTC
+        $checkInDatetime = Carbon::parse($request->input('check_in_datetime'), 'Asia/Manila')->utc();
+        $expectedCheckoutDateTime = $checkInDatetime->addHours((int) $request->input('number_of_hours'));
 
         // Fill in the reservation details
         $reservation->fill([
             'reserved_room_id' => $request->input('reserved_room_id'),
             'room_additions' => json_decode($request->input('room_additions'), true),
             'rate_availed_id' => $request->input('rate_availed_id'),
-            'check_in_datetime' => $request->input('check_in_datetime'),
-            'expected_check_out' => $request->input('expected_check_out'),
+            'check_in_datetime' => $checkInDatetime,
+            'expected_check_out' => $expectedCheckoutDateTime,
             'number_of_hours' => $request->input('number_of_hours'),
             'number_of_days' => $request->input('number_of_days'),
             'guest_name' => $request->input('guest_name'),
@@ -36,19 +41,6 @@ class ReservationController extends Controller
 
         // Save the reservation (either create or update)
         $reservation->save();
-
-        // Get the existing room_reservation_ids or initialize an empty array
-        $reservationIds = $room->room_reservation_ids ?? [];
-
-        // Ensure the reservation ID is unique before adding
-        if (!in_array($reservation->id, $reservationIds)) {
-            $reservationIds[] = $reservation->id;
-        }
-
-        // Update the room with the new array
-        $room->update([
-            'room_reservation_ids' => $reservationIds,
-        ]);
 
         return to_route('frontdesk');
     }

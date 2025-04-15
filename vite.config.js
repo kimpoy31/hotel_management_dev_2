7b265093ceb09e3190f0442b5af3a6e8782b8f01
kimpoy <kimpoyyy31@gmail.com>
2025-04-15 23:29:50 +0800

    correctly handle vite.config.js app_host

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,29 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: "resources/js/app.tsx",
-            refresh: true,
-        }),
-        react(),
-        tailwindcss(),
-    ],
-    server: {
-        host: "192.168.1.6",
-        port: 5173,
-        strictPort: true,
-        hmr: {
-            protocol: "ws",
-            host: "192.168.1.6",
+export default defineConfig(({ mode }) => {
+    // Load .env based on the current mode (e.g., development, production)
+    const env = loadEnv(mode, process.cwd(), "");
+
+    return {
+        plugins: [
+            laravel({
+                input: "resources/js/app.tsx",
+                refresh: true,
+            }),
+            react(),
+            tailwindcss(),
+        ],
+        server: {
+            host: env.APP_HOST,
+            port: 5173,
+            strictPort: true,
+            hmr: {
+                protocol: "ws",
+                host: env.APP_HOST,
+            },
         },
-    },
+    };
 });
